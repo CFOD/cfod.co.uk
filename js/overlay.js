0ae9f9053b554ffdec21fa43c2b5cffd4fcff313
Matt Hoyle <matt@hoyle.dev>
2024-03-14 21:52:02 +0000

    overlay script

---
+++ js/overlay.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    if (!localStorage.getItem("Acknowledged")) {
+        document.body.innerHTML += `
+        <div id="overlay">
+            <div id="overlay-content">
+                <h2>Website Under Development</h2>
+                <p>This website is currently under development. All elements and content are works in progress.</p>
+                <a href="#acknowledge" id="acknowledge-link" onclick="document.querySelector('#overlay').style.display='none'">I Understand</a>
+            </div>
+        </div>`;
+
+        document.querySelector('#overlay').addEventListener("click", () => {
+            document.querySelector('#overlay').style.display='none';
+            localStorage.setItem("Acknowledged", true);
+        });
+    }
+});
